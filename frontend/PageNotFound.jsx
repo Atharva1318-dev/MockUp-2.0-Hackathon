0dc8abb173e6962f86eb3560b08f86e0f580e679
Atharva <atharvajofficial@email.com>
2025-03-10 22:31:23 +0530

    initial

---
+++ frontend/PageNotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+function PageNotFound() {
+    return (
+        <div className="container">
+            <DotLottieReact
+                src="https://lottie.host/81f68897-ff93-4a17-81c4-1f59e048f742/niesnGPGql.lottie"
+                loop
+                autoplay
+            />
+        </div>
+    );
+}
+
+export default PageNotFound;
+
